096e4b4d7c8b2f19ca31965d7ef4af00f0d89146
Justin "Judd" Fowler <65981023+GitJudd@users.noreply.github.com>
2020-10-15 21:28:23 -0500

    Add files via upload

--- js/pokedexapp3.js
+++ js/pokedexapp3.js
@@ -2,13 +2,14 @@ const pokedex = document.getElementById('pokedex');
 
 const fetchPokemon = () => {
     const promises = [];
-    for (let i = 1; i <= 151; i++) {
+    for (let i = 252; i <= 386; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(url).then((res) => res.json()));
     }
     Promise.all(promises).then((results) => {
         const pokemon = results.map((result) => ({
             name: result.name,
+            image: result.sprites['front_default'],
             type: result.types.map((type) => type.type.name).join(', '),
             id: result.id
         }));
@@ -22,7 +23,7 @@ const displayPokemon = (pokemon) => {
         .map(
             (pokeData) => `
         <li class="card">
-        <img class="card-image" src="https://github.com/GitJudd/teamtriangle/blob/main/pokedex/${pokeData.id}.png?raw=true"/>
+            <img class="card-image" src="${pokeData.image}"/>
             <h2 class="card-title">${pokeData.id}. ${pokeData.name}</h2>
             <p class="card-subtitle">Type: ${pokeData.type}</p>
         </li>
